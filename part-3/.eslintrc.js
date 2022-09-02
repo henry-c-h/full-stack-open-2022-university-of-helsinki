91dc0eea20fc4a9051cfe66e2d52144a4788ba66
henrychen <henrychen91@outlook.com>
2022-09-02 20:42:42 +0100

    add eslint

---
+++ part-3/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    eqeqeq: 'error',
+    'no-console': 0,
+    'no-unused-vars': 0,
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+  },
+};
